fe5004fe9a699cce7a9a7b9a7498dbc28b922a26
Manoj Royal <manojroyal7805@gmail.com>
2026-01-31 10:53:19 +0530

    Ensured matrix + repo names are sent to UI

--- app/services.py/report_generator.py
+++ app/services.py/report_generator.py
@@ -1,24 +1,31 @@
+import os
 from app.services.github_service import clone_repositories
 from app.services.preprocessing import preprocess_code
 from app.services.similarity import compute_similarity
 
 def generate_report(repo_urls, language, threshold):
-    paths = clone_repositories(repo_urls)
+    repo_paths = clone_repositories(repo_urls)
 
     documents = []
-    repo_map = []
+    repo_names = []
 
-    for path in paths:
+    for path in repo_paths:
+        repo_name = os.path.basename(path)
         for root, _, files in os.walk(path):
-            for f in files:
-                if f.endswith(language):
-                    with open(os.path.join(root, f), "r", errors="ignore") as file:
-                        documents.append(preprocess_code(file.read()))
-                        repo_map.append(path)
+            for file in files:
+                if file.endswith(language):
+                    file_path = os.path.join(root, file)
+                    try:
+                        with open(file_path, "r", errors="ignore") as f:
+                            documents.append(preprocess_code(f.read()))
+                            repo_names.append(repo_name)
+                    except:
+                        continue
 
     similarity_matrix = compute_similarity(documents)
 
     return {
+        "repositories": repo_names,
         "similarity_matrix": similarity_matrix.tolist(),
-        "repositories": repo_urls
+        "flagged_pairs": []  # will fill later
     }
